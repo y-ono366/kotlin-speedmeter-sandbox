eb1a35831e98ec3a30092b5d1744a92d404498fe
Yusuke Ono <y.ono.varial@gmail.com>
2018-06-25 20:25:26 +0900

    これがマジ最初のコミットぱねぇ

---
+++ app/src/main/java/systems/ohno/h_money/MainActivity.kt
@@ -0,0 +1,12 @@
+package systems.ohno.h_money
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
