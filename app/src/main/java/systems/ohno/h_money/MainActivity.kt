f73a229ed09eafe00b42316167c2e71cb37eeac3
Yusuke Ono <y.ono.varial@gmail.com>
2018-06-30 20:45:58 +0900

    無駄なファイル削除した。

--- app/src/main/java/systems/ohno/h_money/MainActivity.kt
+++ app/src/main/java/systems/ohno/h_money/MainActivity.kt
@@ -2,11 +2,13 @@ package systems.ohno.h_money
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import systems.ohno.h_money.topView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        topView().render()
     }
 }
---
+++ app/src/main/java/systems/ohno/h_money/view.kt
@@ -0,0 +1,11 @@
+package systems.ohno.h_money
+
+
+import android.widget.FrameLayout
+
+
+class topView {
+  fun render() {
+  }
+}
+
