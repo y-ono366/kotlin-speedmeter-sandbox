dd6f14f9c00a1839cc7892c4ddc2298f52f1a3e9
Yusuke Ono <y.ono.varial@gmail.com>
2018-07-18 20:25:50 +0900

    ボタン機能追加

--- app/src/main/java/systems/ohno/h_money/MainActivity.kt
+++ app/src/main/java/systems/ohno/h_money/MainActivity.kt
@@ -8,26 +8,65 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Calendar
 import java.text.SimpleDateFormat
-
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super<AppCompatActivity>.onCreate(savedInstanceState)
 
         val prefs = getSharedPreferences("HMONEY_FILE", AppCompatActivity.MODE_PRIVATE)
-        val editor = prefs.edit()
-        editor.putLong("total",100000)
-        editor.commit()
-        var total = prefs.getLong("total",0)
 
-        var viewer  = topView(applicationContext)
+        var total = prefs.getLong("total",0)
         var hour_speed = HourCalculation(total)
+        var viewer  = topView(applicationContext)
+
         viewer.setText(Speed(hour_speed))
         setContentView(viewer)
+
+        val oneHundBtn: Button = findViewById(R.id.oneHundBtn)
+        oneHundBtn.setOnClickListener {
+            var newTotal = prefs.getLong("total",0) + 100
+            val editor = prefs.edit()
+            editor.putLong("total",newTotal)
+            editor.commit()
+            viewer.setText(Speed(HourCalculation(newTotal)))
+        }
+
+
+        val fiveHundBtn: Button = findViewById(R.id.fiveHundBtn)
+        fiveHundBtn.setOnClickListener {
+            var newTotal = prefs.getLong("total",0) + 500
+            val editor = prefs.edit()
+            editor.putLong("total",newTotal)
+            editor.commit()
+            viewer.setText(Speed(HourCalculation(newTotal)))
+        }
+
+        val thousandBtn: Button = findViewById(R.id.thousandBtn)
+        thousandBtn.setOnClickListener {
+            var newTotal = prefs.getLong("total",0) + 1000
+            val editor = prefs.edit()
+            editor.putLong("total",newTotal)
+            editor.commit()
+            viewer.setText(Speed(HourCalculation(newTotal)))
+        }
+
+        val fiveThousandBtn: Button = findViewById(R.id.fiveThousandBtn)
+        fiveThousandBtn.setOnClickListener {
+            var newTotal = prefs.getLong("total",0) + 5000
+            val editor = prefs.edit()
+            editor.putLong("total",newTotal)
+            editor.commit()
+            viewer.setText(Speed(HourCalculation(newTotal)))
+        }
+
     }
 
     fun  HourCalculation(speed:Long): Long {
-        var beforeDate = "2018-07-01"
+        var beforeDate = "2018-07-15"
 
         var diffDate =getDiffDay(beforeDate)
 
