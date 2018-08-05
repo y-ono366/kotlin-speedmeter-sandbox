bf54ea3aa112c77f61fb17e55c3922cb552d5881
Yusuke Ono <y.ono.varial@gmail.com>
2018-08-05 20:09:54 +0900

    設定画面のlayoutを変更

--- app/src/main/java/systems/ohno/h_money/SettingActivity.kt
+++ app/src/main/java/systems/ohno/h_money/SettingActivity.kt
@@ -1,13 +1,9 @@
 package systems.ohno.h_money
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.app.Activity
 import android.widget.Button
 import android.content.Intent
-
-import kotlinx.android.synthetic.main.activity_setting.*
-import org.w3c.dom.Text
 import android.text.InputType
 import android.widget.EditText
 
@@ -17,31 +13,31 @@ class SettingActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        val payDay= findViewById(R.id.payDay) as EditText
-        payDay.setInputType(InputType.TYPE_CLASS_NUMBER)
-        val amount= findViewById(R.id.amount) as EditText
-        amount.setInputType(InputType.TYPE_CLASS_NUMBER)
-
-        val prefs = getSharedPreferences("HMONEY_FILE", Activity.MODE_PRIVATE)
-        var prefAmount = prefs.getInt("amount",0)
-        var prefPayDay = prefs.getInt("payDay",1)
-
-        payDay.setText(prefPayDay.toString())
-        amount.setText(prefAmount.toString())
-
-
-        val saveBtn:Button = findViewById(R.id.saveBtn) as Button
-        saveBtn.setOnClickListener {
-            val day = Integer.parseInt(payDay.text.toString())
-            val amount = Integer.parseInt(amount.text.toString())
-
-            val editor = prefs.edit()
-            editor.putInt("payDay",day)
-            editor.putInt("amount",amount)
-            editor.commit()
-            val settingIntent = Intent(this, MainActivity::class.java)
-            startActivity(settingIntent)
-        }
+//        val payDay= findViewById(R.id.payDay) as EditText
+//        payDay.setInputType(InputType.TYPE_CLASS_NUMBER)
+//        val amount= findViewById(R.id.amount) as EditText
+//        amount.setInputType(InputType.TYPE_CLASS_NUMBER)
+//
+//        val prefs = getSharedPreferences("HMONEY_FILE", Activity.MODE_PRIVATE)
+//        var prefAmount = prefs.getInt("amount",0)
+//        var prefPayDay = prefs.getInt("payDay",1)
+//
+//        payDay.setText(prefPayDay.toString())
+//        amount.setText(prefAmount.toString())
+//
+//
+//        val saveBtn:Button = findViewById(R.id.saveBtn) as Button
+//        saveBtn.setOnClickListener {
+//            val day = Integer.parseInt(payDay.text.toString())
+//            val amount = Integer.parseInt(amount.text.toString())
+//
+//            val editor = prefs.edit()
+//            editor.putInt("payDay",day)
+//            editor.putInt("amount",amount)
+//            editor.commit()
+//            val settingIntent = Intent(this, MainActivity::class.java)
+//            startActivity(settingIntent)
+//        }
 
     }
 
